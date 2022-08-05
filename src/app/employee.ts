750d3ee33b543ca95f3ff0b76770b6e9e962aaf8
Nawid Shadab <shadab.nawid@yahoo.com>
2022-08-06 01:29:26 +0200

    first commit of EmployeeManagerApp with Angular

---
+++ src/app/employee.ts
@@ -0,0 +1,12 @@
+
+// mirroring how the employee looks like in the backenend in this interface
+export interface Employee {
+  id: number;
+  name: string;
+  email: string;
+  jobTitle: string;
+  phone: string;
+  imageUrl: string;
+  employeeCode: string;
+
+}
